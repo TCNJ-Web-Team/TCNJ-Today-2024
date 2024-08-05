27a5ba7095f31082cf36df063ae2fad46a5a46c3
lvl12sealclubber <adam.holsten@gmail.com>
2024-08-05 10:42:47 -0400

    Update AppListing.tsx

--- src/AppListing.tsx
+++ src/AppListing.tsx
@@ -31,12 +31,14 @@ const AppListing: React.FC<AppListingProps> = ({ appList }) => {
             exit={{ opacity: 0, y: -10 }}
             id="app-list"
             className="absolute top-0 left-0 w-[100%] max-h-[100%] bg-[rgba(255,255,255,0.95)]
-      grid grid-cols-2  shadow-3xl border-[1px] border-[#e0e0e0]
+      grid grid-cols-2  
+      sm:shadow-3xl 
+      sm:border-[1px] sm:border-[#e0e0e0]
       pt-[47px] pb-[200px]
       gap-y-[60px]  
       sm:top-[10px] sm:grid-cols-3  sm:w-[655px] sm:h-[708px]
       md:w-[630px] md:max-h-[708px]  md:left-auto md:right-0
-      overflow-auto"
+      sm:overflow-auto"
           >
             {appList.map((item) => (
               <motion.a
