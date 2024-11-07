75f8cf11a5f6787594508db99b531d557b54799d
lvl12sealclubber <adam.holsten@gmail.com>
2024-11-07 13:45:56 -0500

    lower border hover

--- src/TopNav.tsx
+++ src/TopNav.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TCNJ_URL } from "../global";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface TopNavProps {
   topNavItems: { id: number; name: string; url: string; topNav: boolean }[];
@@ -30,17 +31,20 @@ const TopNav: React.FC<TopNavProps> = ({ topNavItems }) => {
   };
   // console.log(showSiteMenu);
   return (
-    <div className="flex justify-end gap-[5px] lg:gap-[26px] items-center">
+    <div className="flex justify-end gap-[15px] lg:gap-[36px] items-center">
       {/* {showSiteMenu && <p>Site Menu</p>}
       {showAppMenu && <p>Site App Menu</p>} */}
       {topNavItems.map((item, index) => (
-        <a
+        // Inside your map function, modify the motion.a and motion.span like this:
+        <motion.a
           className="
-          md:block hidden cursor-pointer
-          text-center leading-[35px] text-[14px] text-[600] font-opensans text-[#000000] pt-[10px] px-[10px] pb-[2.5px]
-           hover:bg-white transition-all duration-500"
+    md:block hidden cursor-pointer relative
+    text-center leading-[35px] text-[14px] text-[600] font-opensans text-[#000000] pt-[10px] px-[0px] pb-[2.5px]
+    transition-all duration-500"
           href={item.url}
           key={item.id}
+          initial="initial"
+          whileHover="hover" // This tells framer-motion to use the hover variant when the element is hovered
         >
           <img
             src={`${TCNJ_URL}/icons/${item.name
@@ -49,9 +53,26 @@ const TopNav: React.FC<TopNavProps> = ({ topNavItems }) => {
             alt={item.name}
             className="h-[25px] mx-auto"
           />
-
           {item.name}
-        </a>
+          <motion.span
+            className={`absolute bottom-[2px] left-0 h-[1.5px] 
+    hidden sm:block`}
+            variants={{
+              // Define the animation variants
+              initial: {
+                width: 0,
+                backgroundColor: "#000",
+                opacity: 0,
+              },
+              hover: {
+                width: "100%",
+                backgroundColor: "#000",
+                opacity: 1,
+              },
+            }}
+            transition={{ duration: 0.35, ease: "easeInOut" }}
+          />
+        </motion.a>
       ))}
       <a
         className="
